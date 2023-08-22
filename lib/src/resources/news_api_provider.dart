32299cdbdf380ddd80082f68697bd1f8c9ae0ae6
rubayet211 <rubayet211@gmail.com>
2023-08-22 20:34:03 +0600

    2nd Commit

---
+++ lib/src/resources/news_api_provider.dart
@@ -0,0 +1,28 @@
+import 'package:http/http.dart' show Client;
+import 'dart:async';
+import 'dart:convert';
+import '../models/item_model.dart';
+
+final _root = 'https://hacker-news.firebaseio.com/v0';
+
+class NewAPIProvider {
+  Client client = Client();
+
+  fetchTopIds() async {
+    final String topIdsUrl = '$_root/topstories.json';
+    final Uri uri = Uri.parse(topIdsUrl);
+    final response = await client.get(uri);
+    final ids = json.decode(response.body);
+
+    return ids;
+  }
+
+  fetchItem(int id) async {
+    final String itemUrl = '$_root/item/$id.json';
+    final Uri uri = Uri.parse(itemUrl);
+    final response = await client.get(uri);
+    final parsedJson = json.decode(response.body);
+
+    return ItemModel.fromJson(parsedJson);
+  }
+}
---
+++ test/src/resources/news_api_provider_test.dart
@@ -0,0 +1,1 @@
+import 'package: news/lib/src/resources/news_api_provider.dart';

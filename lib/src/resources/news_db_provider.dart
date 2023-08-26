7cfd6fadd7fdb0a5af2890e3d590bd8d289390ad
rubayet211 <rubayet211@gmail.com>
2023-08-27 00:11:38 +0600

    7Th Commit

--- lib/src/resources/news_db_provider.dart
+++ lib/src/resources/news_db_provider.dart
@@ -11,7 +11,35 @@ class NewsDbProvider {
   init() async {
     Directory documentsDirectory = await getApplicationDocumentsDirectory();
     final path = join(documentsDirectory.path, 'items.db');
-    db = await openDatabase(path,
-        version: 1, onCreate: (Database newDb, int version) {});
+    db = await openDatabase(path, version: 1,
+        onCreate: (Database newDb, int version) {
+      newDb.execute("""
+        CREATE TABLE Items
+          (
+            id INTEGER PRIMARY KEY,
+            type Text,
+            by Text,
+            time INTEGER,
+            text Text,
+            parent INTEGER,
+            kids BLOB,
+            dead INTEGER,
+            deleted INTEGER,
+            url Text,
+            score INTEGER,
+            title Text,
+            descendants INTEGER
+          )
+      """);
+    });
+  }
+
+  fetchItem(int id) async {
+    db.query(
+      "Items",
+      columns: null,
+      where: "id = ?",
+      whereArgs: [id],
+    );
   }
 }

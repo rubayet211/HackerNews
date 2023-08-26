29a589f6dd875c46d50e0840cafbb35546f24289
rubayet211 <rubayet211@gmail.com>
2023-08-26 23:37:29 +0600

    6th Commit

---
+++ lib/src/resources/news_db_provider.dart
@@ -0,0 +1,17 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'package:path/path.dart';
+import 'dart:async';
+import '../models/item_model.dart';
+
+class NewsDbProvider {
+  Database db;
+
+  init() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    final path = join(documentsDirectory.path, 'items.db');
+    db = await openDatabase(path,
+        version: 1, onCreate: (Database newDb, int version) {});
+  }
+}

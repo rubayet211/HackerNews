0c9f77c96857b71e8fa5cdca3d5952b546001f7a
rubayet211 <rubayet211@gmail.com>
2023-09-03 14:31:15 +0600

    19th Commit

---
+++ lib/src/blocs/stories_provider.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'stories_bloc.dart';
+
+class StoriesProvider extends InheritedWidget {
+  final StoriesBloc bloc;
+
+  StoriesProvider({Key? key, Widget? child})
+      : bloc = StoriesBloc(),
+        super(key: key, child: child!);
+
+  bool updateShouldNotify(InheritedWidget oldWidget) => true;
+
+  static StoriesBloc of(BuildContext context) {
+    return (context.dependOnInheritedWidgetOfExactType<StoriesProvider>()
+            as StoriesProvider)
+        .bloc;
+  }
+}
--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -8,33 +8,7 @@ class NewsList extends StatelessWidget {
       appBar: AppBar(
         title: Text('Top News'),
       ),
-      body: buildList(),
-    );
-  }
-
-  Widget buildList() {
-    return ListView.builder(
-      itemCount: 1000,
-      itemBuilder: (context, int index) {
-        return FutureBuilder(
-          future: getFuture(),
-          builder: (context, snapshot) {
-            return Container(
-              height: 80,
-              child: snapshot.hasData
-                  ? Text('Im visible $index')
-                  : Text('I havent fetched data yet $index'),
-            );
-          },
-        );
-      },
-    );
-  }
-
-  getFuture() {
-    return Future.delayed(
-      Duration(seconds: 2),
-      () => 'hi',
+      body: Text('Show a list of news here'),
     );
   }
 }
